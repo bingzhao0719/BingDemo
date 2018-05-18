eb4a1a501ef2c02b93c32230bb60b036d8777a1e
wubz <wubingzhao@richinfo.cn>
2018-05-18 13:32:52 +0800

    添加一个base的模块

---
+++ base/src/main/java/com/bing/demo/base/MainActivity.java
@@ -0,0 +1,12 @@
+package com.bing.demo.base;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
